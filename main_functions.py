0ed1f07a00998674a9db82958141b9cc6af6c4ea
MikhaGitHub <https://github.com/MikhaGitHub/Parcing-data-from-football-matches>
2025-01-13 19:37:58 +0300

    Change main_functions.py
    Change processing data, add in arguments of function ```payload=None```

--- main_functions.py
+++ main_functions.py
@@ -3,13 +3,15 @@ import os
 import requests
 
 
-def download_img(url,filename, directory):
-    Path(f"/{directory}").mkdir(parents=True, exist_ok=True)
-    response = requests.get(url)
+def download_img(url,filename, folder, payload=None):
+    response = requests.get(url, params=payload, stream=True)
     response.raise_for_status()
-    
-    with open(f'{directory}/{filename}', 'wb') as file:
-        file.write(response.content)
+    os.makedirs(folder, exist_ok=True)
+    filepath = os.path.join(folder, filename)
+
+    with open(filepath, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
 
 
 def get_extension_of_file(url):
