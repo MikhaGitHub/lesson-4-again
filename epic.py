923e37c04a7882013b7bf363e600f5ee6f921f57
MikhaGitHub <https://github.com/MikhaGitHub/Parcing-data-from-football-matches>
2025-01-14 16:45:51 +0300

    change epic.py
    del exeptions

--- epic.py
+++ epic.py
@@ -8,31 +8,27 @@ import argparse
 from datetime import datetime
 
 def main():
-    try:
-        load_dotenv()
-        parser = argparse.ArgumentParser(description="This code download images from Apod NASA in your folder")
-        parser.add_argument('--folder', type=str, default="images", help="Enter name of your exesting folder")
-        args = parser.parse_args()
-        folder = args.folder
-        api_key_epic = os.getenv("API_KEY")
-        payload = {
-            "api_key": api_key_epic,
-        }
-        base_url = f"https://api.nasa.gov/EPIC/api/natural/"
-        response = requests.get(base_url, params=payload)
-        response.raise_for_status()
-        for img_count, img in enumerate(response.json()):
-            img_identifier = img["image"]
-            img_date = str(img["date"])
-            img_date_split = img_date.split()[0]
-            parts = img_date_split.split('-')
-            url_date = "/".join(parts)
-            url = f'https://api.nasa.gov/EPIC/archive/natural/{url_date}/png/{img_identifier}.png'
-            filename = f'epic_{img_count}.png'
-            download_img(url, filename, folder, payload)
-
-    except requests.exceptions.HTTPError as err:
-        print(f'Возможно вы ввели неправильный токен или неверную ссылку,измените содержимое запроса и попробуйте снова, Error HTTP:{err}')
+    load_dotenv()
+    parser = argparse.ArgumentParser(description="This code download images from Apod NASA in your folder")
+    parser.add_argument('--folder', type=str, default="images", help="Enter name of your exesting folder")
+    args = parser.parse_args()
+    folder = args.folder
+    api_key_epic = os.getenv("API_KEY_NASA")
+    payload = {
+        "api_key": api_key_epic,
+    }
+    base_url = f"https://api.nasa.gov/EPIC/api/natural/"
+    response = requests.get(base_url, params=payload)
+    response.raise_for_status()
+    for img_count, img in enumerate(response.json()):
+        img_identifier = img["image"]
+        img_date = str(img["date"])
+        img_date_split = img_date.split()[0]
+        parts = img_date_split.split('-')
+        url_date = "/".join(parts)
+        url = f'https://api.nasa.gov/EPIC/archive/natural/{url_date}/png/{img_identifier}.png'
+        filename = f'epic_{img_count}.png'
+        download_img(url, filename, folder, payload)
 
 
 if __name__ == "__main__":
