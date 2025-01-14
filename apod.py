9af30194932d20392324170d2c72373dbe6dba71
MikhaGitHub <https://github.com/MikhaGitHub/Parcing-data-from-football-matches>
2025-01-14 16:45:17 +0300

    change apod.py
    delete exceptions

--- apod.py
+++ apod.py
@@ -6,37 +6,32 @@ import argparse
 
 
 def main():
-    try:
-        load_dotenv()
-        api_key_nasa_apod = os.getenv("API_KEY")
-        parser = argparse.ArgumentParser(description="This code download images from Apod NASA in your folder")
-        parser.add_argument('--count', type=int, default=26, help="enter limit of images")
-        parser.add_argument('--folder', type=str, default="images", help="Enter name of your exesting folder")
-        args = parser.parse_args()
-        limit = args.count
-        folder = args.folder
+    load_dotenv()
+    api_key_nasa_apod = os.getenv("API_KEY_NASA")
+    parser = argparse.ArgumentParser(description="This code download images from Apod NASA in your folder")
+    parser.add_argument('--count', type=int, default=26, help="enter limit of images")
+    parser.add_argument('--folder', type=str, default="images", help="Enter name of your exesting folder")
+    args = parser.parse_args()
+    limit = args.count
+    folder = args.folder
         
         
-        params = {
-            "count":limit,
-            "api_key":api_key_nasa_apod
-        }
+    params = {
+        "count":limit,
+        "api_key":api_key_nasa_apod
+    }
 
 
-        full_url = f"https://api.nasa.gov/planetary/apod"
+    full_url = f"https://api.nasa.gov/planetary/apod"
 
-        response_apod = requests.get(full_url, params=params)
-        response_apod.raise_for_status()
-        for index_img, img in enumerate(response_apod.json()):
-            url = img['url']
-            extension = get_extension_of_file(url)
-            filename = f'nasa_apod_{index_img}{extension[0]}'
-            download_img(url,filename, folder)
-    except requests.exceptions.HTTPError as err:
-        print(f'Возможно вы ввели неправильный токен или неверную ссылку,измените содержимое запроса и попробуйте снова, Error HTTP:{err}')
+    apod_response = requests.get(full_url, params=params)
+    apod_response.raise_for_status()
+    for index_img, img in enumerate(apod_response.json()):
+        url = img['url']
+        extension = get_extension_of_file(url)
+        filename = f'nasa_apod_{index_img}{extension[0]}'
+        download_img(url,filename, folder)
 
 
 if __name__ == "__main__":
      main()
-
-
